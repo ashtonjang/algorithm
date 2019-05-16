c6d7c5dfb7af96e18039692851c77b16c3a4ef6b
JangSeungHyun <ashton2.3jang@gmail.com>
2019-05-16 17:22:12 +0900

    완전탐색 -> 카펫 완료

--- programmers/BruteForceSearch/Carpet.js
+++ programmers/BruteForceSearch/Carpet.js
@@ -4,10 +4,22 @@
  */
 
 console.log("Test1:", solution(10, 2));
-console.log("Test2:", solution(8, 1));
-console.log("Test3:", solution(24, 24));
+//console.log("Test2:", solution(8, 1));
+//console.log("Test3:", solution(24, 24));
 
+/**
+ * x + y = brown
+ * (x - 2) * y = red
+ */
 function solution(brown, red) {
-    var answer = [];
-    return answer;
+    let y = 0;
+    let result;
+    while (++y && y < brown) {
+        const x = (brown / 2) - y;
+        if ((x - 2) * y === red) {
+            result = [x, y + 2];
+            break;
+        }
+    }
+    return result;
 }
